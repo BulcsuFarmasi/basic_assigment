79828145f1f0ca646b2f9cc503fd6fced7977762
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-12-27 18:25:57 +0100

    bind event listener to button

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,13 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   String appText =
       'I am the original  text of the app, whom the developer of the app wants to change, and I don\'t want it, but he still gonna do it.';
+
+  _changeText() {
+    setState(() {
+      appText = 'I am the new text and I am deeply sorry that the previous text was changed';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,7 +31,10 @@ class _AppState extends State<App> {
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 24),
             ),
-            RaisedButton(onPressed: null, child: Text('Change text anyway'),)
+            RaisedButton(
+              onPressed: _changeText,
+              child: Text('Change text anyway'),
+            )
           ],
         ),
       ),
